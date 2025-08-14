d3e19dea514ee1d1040b6d181c597c261f6db5f3
Liu <Ying.Liu@tabcorp.com.au>
2025-08-14 11:21:11 +1000

    update commonet

--- terraform/apps/alb_controller.tf
+++ terraform/apps/alb_controller.tf
@@ -1,10 +1,10 @@
 resource "aws_iam_policy" "alb_policy" {
-  name        = "AWSLoadBalancerControllerIAMPolicy"
-  path        = "/"
+  name   = "AWSLoadBalancerControllerIAMPolicy"
+  path   = "/"
   policy = file("iam_policy.json")
 }
 module "iam_assumable_role_with_oidc" {
-  source = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
+  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
   version = "~> 2.0"
 
   create_role = true
@@ -15,18 +15,18 @@ module "iam_assumable_role_with_oidc" {
     Role = "role-alb-controller"
   }
 
-  provider_url = "oidc.eks.ap-south-1.amazonaws.com/id/48D441B543341965034A0A4E4F5CCAE5"
+  provider_url = "oidc.eks.ap-south-2.amazonaws.com/id/48D441B543341965034A0A4E4F5CCAE5"
 
   role_policy_arns = [
     aws_iam_policy.alb_policy.arn,
   ]
 }
 
-module alb_controller {
-  source  = "../modules/alb_controller"
+module "alb_controller" {
+  source = "../modules/alb_controller"
 
   namespace  = "kube-system"
-  repository =  "https://aws.github.io/eks-charts"
+  repository = "https://aws.github.io/eks-charts"
 
   app = {
     name          = "aws-load-balancer-controller"
--- terraform/apps/variables.tf
+++ terraform/apps/variables.tf
@@ -1,3 +1,3 @@
 variable "idp_provider_url" {
-  default = "oidc.eks.ap-south-1.amazonaws.com/id/48D441B543341965034A0A4E4F5CCAE5"
+  default = "oidc.eks.ap-south-2.amazonaws.com/id/48D441B543341965034A0A4E4F5CCAE5"
 }
