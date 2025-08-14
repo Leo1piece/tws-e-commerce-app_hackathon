897f840e4a4fb7d9d161a7a74ef5764ab572faac
Liu <Ying.Liu@tabcorp.com.au>
2025-08-14 12:05:26 +1000

    update ap south -1 to ap-southeast-2

--- terraform/apps/terraform.tf
+++ terraform/apps/terraform.tf
@@ -9,7 +9,7 @@ terraform {
       version = ">=2.17"
     }
     kubernetes = {
-      source = "hashicorp/kubernetes"
+      source  = "hashicorp/kubernetes"
       version = "2.37.1"
     }
   }
@@ -28,6 +28,7 @@ provider "helm" {
 }
 
 provider "kubernetes" {
-  config_path    = "~/.kube/config"
+  config_path = "~/.kube/config"
 }
 
+# 3 bucket 是全局唯一，但每个 bucket 有自己的 region。你必须让 Terraform 的 backend 配置里的 region 和 S3 bucket 的实际 region 一致，否则会报 301 重定向错误
